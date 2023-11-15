14187a9872e8e82b3f32d54ddd99b7e5e49f8b00
vemundtl <vemund@thallaug.org>
2023-11-15 13:30:16 +0100

    init

---
+++ datasets.py
@@ -0,0 +1,65 @@
+import os
+from PIL import Image 
+import torch
+from torch.utils.data import Dataset
+from torchvision import transforms, datasets
+import numpy as np
+import os
+
+class CityscapesDataset(Dataset):
+    def __init__(self, split, root_dir, target_type='semantic', mode='fine', transform=None, eval=False, relabelled=True):
+        self.transform = transform
+        if mode == 'fine':
+            self.mode = 'gtFine'
+        elif mode == 'coarse':
+            self.mode = 'gtCoarse'
+        self.split = split
+        self.yLabel_list = []
+        self.XImg_list = []
+        self.eval = eval
+
+        # Preparing a list of all labelTrainIds rgb and 
+        # ground truth images. Setting relabbelled=True is recommended. 
+
+        self.label_path = os.path.join(os.getcwd(), root_dir+'/'+self.mode+'/'+self.split)
+        self.rgb_path = os.path.join(os.getcwd(), root_dir+'/leftImg8bit/'+self.split)
+        city_list = os.listdir(self.label_path)
+        for city in city_list:
+            temp = os.listdir(self.label_path+'/'+city)
+            list_items = temp.copy()
+    
+            # 19-class label items being filtered
+            for item in temp:
+                if not item.endswith('labelTrainIds.png', 0, len(item)):
+                    list_items.remove(item)
+
+            # defining paths
+            list_items = ['/'+city+'/'+path for path in list_items]
+
+            self.yLabel_list.extend(list_items)
+            self.XImg_list.extend(
+                ['/'+city+'/'+path for path in os.listdir(self.rgb_path+'/'+city)]
+            )
+                
+    def __len__(self):
+        length = len(self.XImg_list)
+        return length
+
+
+    def __getitem__(self, index):
+        image = Image.open(self.rgb_path+self.XImg_list[index])
+        y = Image.open(self.label_path+self.yLabel_list[index])
+
+        if self.transform is not None:
+            image = self.transform(image)
+            y = self.transform(y)
+
+        image = transforms.ToTensor()(image)
+        y = np.array(y)
+        y = torch.from_numpy(y)
+        
+        y = y.type(torch.LongTensor)
+        if self.eval:
+            return image, y, self.XImg_list[index]
+        else:
+            return image, y
---
+++ utils.py
@@ -0,0 +1,35 @@
+import torch
+from torch.utils.data import DataLoader
+from torchvision import datasets, utils, transforms
+from datasets import CityscapesDataset
+from PIL import Image
+from tqdm import tqdm
+import numpy as np
+
+def get_cityscapes_data(
+    mode,
+    split,
+    relabelled,
+    root_dir='datasets/cityscapes',
+    target_type="semantic",
+    transforms=None,
+    batch_size=1,
+    eval=False,
+    shuffle=True,
+    pin_memory=True,
+
+):
+    data = CityscapesDataset(
+        mode=mode, split=split, target_type=target_type, relabelled=relabelled, transform=transforms, root_dir=root_dir, eval=eval)
+
+    data_loaded = torch.utils.data.DataLoader(
+        data, batch_size=batch_size, shuffle=shuffle, pin_memory=pin_memory)
+
+    return data_loaded
+
+# Functions to save predictions as images 
+def save_as_images(tensor_pred, folder, image_name):
+    tensor_pred = transforms.ToPILImage()(tensor_pred.byte())
+    filename = f"{folder}/{image_name}.png"
+    tensor_pred.save(filename)
+        
